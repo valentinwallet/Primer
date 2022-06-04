9a6647f61f8efa50f722f5ed43afcb9e360418dc
Valentin Wallet <valentinwallet@gmail.com>
2022-06-04 22:46:12 +0200

    Add extension for string to check if a string only contains number or letters

---
+++ Sources/Primer/Extensions/String+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Valentin Wallet on 6/4/22.
+//
+
+import Foundation
+
+extension String {
+    var onlyContainsLetters: Bool {
+        guard self.count > 0 else { return false }
+        return CharacterSet.letters.isSuperset(of: CharacterSet(charactersIn: self))
+    }
+
+    var onlyContainsNumbers: Bool {
+        guard self.count > 0 else { return false }
+        return CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: self))
+    }
+}
---
+++ Tests/PrimerTests/Extensions/StringExtensionTests.swift
@@ -0,0 +1,44 @@
+//
+//  StringExtensionTests.swift
+//  
+//
+//  Created by Valentin Wallet on 6/4/22.
+//
+
+import XCTest
+@testable import Primer
+
+final class StringExtensionTests: XCTestCase {
+    func test_only_contains_letter() {
+        // GIVEN
+        let testData: [(string: String, expectedResult: Bool)] = [
+            ("", false),
+            ("abc123", false),
+            ("231", false),
+            ("jfiejwfwe", true),
+            ("fijfoiweDEOJJ", true)
+        ]
+
+        // WHEN
+        testData.forEach { (string: String, expectedResult: Bool) in
+            // THEN
+            XCTAssertEqual(string.onlyContainsLetters, expectedResult, "wrong result for string: \(string), should be: \(expectedResult)")
+        }
+    }
+
+    func test_only_contains_numbers() {
+        // GIVEN
+        let testData: [(string: String, expectedResult: Bool)] = [
+            ("", false),
+            ("abc123", false),
+            ("abc", false),
+            ("123", true),
+            ("321342423", true)
+        ]
+
+        // WHEN
+        testData.forEach { (string: String, expectedResult: Bool) in
+            XCTAssertEqual(string.onlyContainsNumbers, expectedResult, "wrong result for string: \(string), should be: \(expectedResult)")
+        }
+    }
+}
