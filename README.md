b5f83b27e74d5ff6d02f9d422391c68499d7e979
Valentin Wallet <valentinwallet@gmail.com>
2022-06-04 16:29:57 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Primer
+
+A description of this package.
---
+++ Tests/PrimerTests/PrimerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Primer
+
+final class PrimerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Primer().text, "Hello, World!")
+    }
+}
